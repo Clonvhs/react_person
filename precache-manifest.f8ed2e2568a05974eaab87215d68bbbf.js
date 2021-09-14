0b6b772c01182a4b8ce2b7fa5b3ff53d7e17e22d
Clon <clonvhs@gmail.com>
2021-09-14 09:13:17 +0300

    Updates

---
+++ precache-manifest.f8ed2e2568a05974eaab87215d68bbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29abcebd852dbdde41f3df6b5375cb51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30711a52fcb6bcbaf43e",
+    "url": "./static/css/main.76f69908.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30711a52fcb6bcbaf43e",
+    "url": "./static/js/main.f29413d4.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
